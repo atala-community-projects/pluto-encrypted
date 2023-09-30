9cea867270a132e41af283bd7b9f9dabc194cca0
Javier Ribó <elribonazo@gmail.com>
2023-09-30 22:53:48 +0200

    fix: build. (#3)
    * fix: build.

--- release.config.js
+++ release.config.js
@@ -2,13 +2,20 @@ export default {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
     "@semantic-release/npm",
     [
       "@semantic-release/git",
       {
-        assets: ["package.json"],
+        assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        signedCommit: true,
       },
     ],
     "@semantic-release/github",
