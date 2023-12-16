69079507af660b6be7bff14f8e6b115a35520544
Javier Ribó <elribonazo@gmail.com>
2023-12-16 13:15:38 +0100

    fix: qa levels and ci improvements (#50)
    * fix: add vitest coverage v8.
    Signed-off-by: Francisco Javier Ribó Labrador <elribonazo@gmail.com>
    
    * fix: update vitest configuration.
    Signed-off-by: Francisco Javier Ribó Labrador <elribonazo@gmail.com>
    
    * chore(docs): Rebuild docs [skip ci]
    
    * chore(release): move release config to the root of the project.
    Signed-off-by: Francisco Javier Ribó Labrador <elribonazo@gmail.com>
    
    * chore(docs): Rebuild docs + coverage [skip ci]

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,8 +5,14 @@ export default defineConfig({
   test: {
     reporters: ['default'], // or 'verbose'
     coverage: {
-      provider: 'istanbul',
-      reporter: ['json-summary']
-    }
+      provider: 'v8',
+      reporter: ['json-summary', 'html'],
+      thresholds: {
+        branches: 58,
+        functions: 64,
+        lines: 74,
+        statements: 71
+      }
+    },
   }
 })
