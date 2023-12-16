cbb1fcf2e33e0a7dacf4a9dcae04972a60efc059
Francisco Javier Ribó Labrador <elribonazo@gmail.com>
2023-12-16 10:04:21 -0500

    fix: fail if coverage doesn't meet threashold. Signed-off-by: Francisco Javier Ribó Labrador <elribonazo@gmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,15 +3,15 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
-    reporters: ['default'], // or 'verbose'
+    reporters: ['verbose'], // or 'verbose'
     coverage: {
       provider: 'v8',
       reporter: ['json-summary'],
       thresholds: {
-        branches: 95,
-        functions: 95,
-        lines: 95,
-        statements: 95
+        branches: 90,
+        functions: 90,
+        lines: 90,
+        statements: 90
       }
     },
   }
